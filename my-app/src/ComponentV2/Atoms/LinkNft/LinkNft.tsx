14556471f7af141f70ca54f8616d0c17446a0752
john <eeee>
2021-05-11 03:04:39 +0200

    [+] ...

--- my-app/src/ComponentV2/Atoms/LinkNft/LinkNft.tsx
+++ my-app/src/ComponentV2/Atoms/LinkNft/LinkNft.tsx
@@ -16,6 +16,7 @@ interface Props {
 }
 
 // redo later
+/*
 const StyledLink = styled.a` 
     margin-top: 32px;
     display: inline-block;
@@ -23,9 +24,11 @@ const StyledLink = styled.a`
     font-weight: 700;
     text-decoration: none;
     cursor: pointer;
-    transition: color 350ms ease;
 
-
+    & > a { 
+        text-decoration : none;
+    }
+   transition: color 350ms ease; 
 
     &:hover {
         color : ${Theme.text.color};
@@ -40,21 +43,56 @@ const StyledLink = styled.a`
 
     & > div:nth-child(2) {
         height : 2px;
-
-       
+        background-image: linear-gradient(90deg, #edb8ff, #6148ff);       
     }
+    */
+/*
+*/
+
+
+const StyledNft = styled.a ` 
+display : inline-block;
+transition: color 350ms ease;
+    color: #0060B6;
+    text-decoration: none;
+    width : 74px;
+    margin-top : 32px;
+    transition: background-color 1s;
+    & > a { 
+        width : 74px;
+
+        text-decoration : none;
+        color: #fff;
+    }
+
+    & > div {
+        margin-top : 8px;
+        width : 74px;
+
+        height : 2px;
+        background-image: linear-gradient(100deg, grey, grey);       
+        transition: background-color 1s;
+    }
+
+    &:hover {
+        transition: background-color 1s;
+    & > div {
+        height : 2px;
+        transition: color 350ms ease;
+        background: rgba(255, 255, 255, 0.24);
+    }    }
+
+
 `;
 
 function LinkNft({href, underline = true}: Props): ReactElement {
     return (
-        <StyledLink>
+        <StyledNft>
             <Link to={href}>
-            <div>View NFT</div>
-            {
-                underline === true && <div></div>
-            }
+                <div>View NFT</div>
             </Link>
-        </StyledLink>
+            <div></div>
+        </StyledNft>
     )
 }
 
